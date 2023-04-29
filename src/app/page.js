65da6ad2a1e88754c232c91101bf33f381c41f2e
t6adev <t.yamamoto28@gmail.com>
2023-04-30 00:57:19 +0900

    add tweets

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
+import { Tweet } from 'react-tweet';
+
 import MyCard from '../components/MyCard';
 import TopBanner from '../components/TopBanner';
 import MenuList from '../components/MenuList';
+
+import { tweetIds } from '../tweets';
 
 const Index = () => {
   return (
@@ -8,6 +12,12 @@ const Index = () => {
       <TopBanner />
       <MyCard />
       <MenuList />
+      <h3 className="text-xl mb-2">My tweets</h3>
+      <div class="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>div:first-child]:mt-0">
+        {tweetIds.map((id) => (
+          <Tweet id={id} key={id} />
+        ))}
+      </div>
     </div>
   );
 };
